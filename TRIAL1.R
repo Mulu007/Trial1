26d2e1780b60e5aa98084386a9746b2b8f73a173
Silvanius <110171336+Mulu007@users.noreply.github.com>
2026-01-18 14:01:18 -0600

    Pivotting in enrollment data

--- TRIAL1.R
+++ TRIAL1.R
@@ -5,3 +5,25 @@ completers <- read_csv("IPEDS Completers Data 2019-2024.csv")
 
 view(enrollment)
 view(completers)
+
+# Pivoting races in enrollment
+glimpse(enrollment)
+
+enrollment_long <- enrollment %>%
+  pivot_longer(
+    cols = c(
+      "American Indian or Alaska Native total",
+      "Asian total",
+      "Black or African American total",
+      "Hispanic or Latino total",
+      "Native Hawaiian or Other Pacific Islander total",
+      "White total",
+      "Two or more races total",
+      "Race/ethnicity unknown total",
+      "U.S. Nonresident total"
+    ),
+    names_to = "Race",
+    values_to = "Enrolled"
+  )
+
+view(enrollment_long)
