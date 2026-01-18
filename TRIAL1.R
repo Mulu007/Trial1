c684d58cbac70323d998fd304db0d1515214b02c
Silvanius <110171336+Mulu007@users.noreply.github.com>
2026-01-18 13:53:45 -0600

    Data importation

---
+++ TRIAL1.R
@@ -0,0 +1,7 @@
+library("tidyverse")
+
+enrollment <- read_csv("IPEDS Enrollment Data 2019-2024 (1).csv")
+completers <- read_csv("IPEDS Completers Data 2019-2024.csv")
+
+view(enrollment)
+view(completers)
